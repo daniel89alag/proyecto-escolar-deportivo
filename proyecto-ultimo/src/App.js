bb09102efcde5b18e4ea5756db48d0738fd45634
daniel <daniel.alvarado@udgvirtual.udg.mx>
2022-03-20 16:24:16 -0600

    seguna modificacion

--- proyecto-ultimo/src/App.js
+++ proyecto-ultimo/src/App.js
@@ -1,19 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <header className="App-header">
-        <h1>prueba</h1>
-        <h1>Empresa deportiva</h1>
+         <h1>Empresa deportiva</h1>
           <ul className="Roc">
-            <li className="dr"><a href="#home">Expedientes</a></li>
+            <li className="dr"><a href="App2.js">Expedientes</a></li>
             <li className="dr"><a href="#news">Bajas</a></li>
             <li className="dr"><a href="#contact">Pagos</a></li>
             <li className="dr"><a href="#about">Reportes</a></li>
           </ul>
-        <img src={logo} className="App-logo" alt="logo" />
+          <div class="general">
+          </div>
+          
         
         
       </header>
